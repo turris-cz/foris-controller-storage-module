ba7f8129a4e3d24c49a8ced16defaa7ecb640b15
Michal Hrusecky <Michal@Hrusecky.net>
2018-02-22 09:29:01 +0100

    Localization of formatting script

--- scripts/format_and_set_srv.sh
+++ scripts/format_and_set_srv.sh
@@ -3,7 +3,7 @@ DISK="$1"
 
 die() {
     echo -e "mkfs: $RES\nuuid: $UUID\nsrv: $SRV\nsrv_dev: $SRV_DEV\nsrv_uuid: $SRV_UUID" > /tmp/storage_debug
-    create_notification -s error "$1" "$1"
+    create_notification -s error "_($1)"
     [ "`cat /tmp/formating`" \!= "$DISK" ] || rm -f /tmp/formating
     exit 2
 }
@@ -18,7 +18,7 @@ if [ -z "$FORKED" ]; then
     export FORKED=1
     if [ -f /tmp/formating ]; then
         DISK="none"
-        die "Another formatting job currently in progress, not switching to $1"
+        die "Another formatting job currently in progress, not switching to new drive."
     fi
     echo "$DISK" > /tmp/formating
     "$0" "$1" &
@@ -35,10 +35,10 @@ if expr "$1" : '^.*[a-z]$' > /dev/null; then
     done
 fi
 RES="$(mkfs.ext4 -q -F -F -L srv -U random -e continue "$1" 2>&1 || echo FAILFAILFAIL)"
-[ -z "$(echo "$RES" | grep FAILFAILFAIL)" ] || die "Formating drive $1 failed, try unpluging it and repluging it in again."
+[ -z "$(echo "$RES" | grep FAILFAILFAIL)" ] || die "Formatting drive failed, try unpluging it and repluging it in again."
 UUID="$(echo "$RES" | sed -n 's|Filesystem UUID: \([0-9a-f-]*\)|\1|p')"
 [ -n "$UUID" ] || UUID="$(blkid "$1" | sed -n 's|.* UUID="\([0-9a-f-]*\)" .*|\1|p')"
-[ -n "$UUID" ] || die "Can't get UUID of your newly formated drive"
+[ -n "$UUID" ] || die "Can't get UUID of your newly formatted drive."
 SRV="$(stat -c %m /srv/)"
 SRV_DEV=""
 [ -z "$SRV" ] || SRV_DEV="$(cat /proc/mounts | sed -n 's|^\(/dev/[^[:blank:]]*\) '"$SRV"' .*|\1|p')"
@@ -52,5 +52,5 @@ if expr "$1" : '^.*[a-z]$' > /dev/null; then
     partx -d "$1"
 fi
 TEXT="Your new disk for storing data has been setup successfully. All you need to do now is to reboot your router for changes to take effect. Be aware that if you have some local data stored on your current storage, those will get moved during reboot, so your next reboot might take quite some time."
-create_notification -s restart "$TEXT" "$TEXT"
+create_notification -s restart "_($TEXT)"
 [ "`cat /tmp/formating`" \!= "$DISK" ] || rm -f /tmp/formating
