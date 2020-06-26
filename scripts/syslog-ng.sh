a386d29e95f8d4be271c13ba41d9c7b2d50a8e6b
Karel Kočí <cynerd@email.cz>
2020-06-26 13:35:17 +0200

    Add syslog-ng configuration to log to /srv/log/messages
    This allows logging on permanent storage in /srv with easy UCI config:
    uci set storage.srv.syslog=1
    
    Syslog-ng is started after srv init so /srv is mounted by that time.

---
+++ scripts/syslog-ng.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+. /lib/functions.sh
+
+config_load "storage"
+config_get uuid "srv" "uuid"
+config_get_bool syslog "srv" "syslog" "1"
+
+# Ignore if storage is not configured
+[ -n "$uuid" ] || return 0
+# /srv has to be mounted
+[ "$(stat -c '%m' /srv)" = "/srv" ] || return 0
+# Syslog has to be enabled for /srv
+[ "$syslog" = "1" ] || return 0
+
+echo "destination(srv_messages)"
