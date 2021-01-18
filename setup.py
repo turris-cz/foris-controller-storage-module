496b99d59bb6eba481b2d9ca0bdf713dbc9a9fc4
Martin Matějek <martin.matejek@nic.cz>
2021-01-19 00:13:26 +0100

    CI: add flake8 to pipeline

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #
 # foris-controller-storage-module
-# Copyright (C) 2018-2020 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2018-2021 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -47,7 +47,10 @@ setup(
     install_requires=[
         "foris-controller @ git+https://gitlab.nic.cz/turris/foris-controller/foris-controller.git#egg=foris-controller"
     ],
-    setup_requires=["pytest-runner"],
+    setup_requires=[
+        "pytest-runner",
+        "flake8"
+    ],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
     dependency_links=[
         "git+https://gitlab.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
